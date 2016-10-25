69fee76d05294d39920b02cec0b953c17f92ca4d
Sergey Hovakimyan <sergey.hovakimyan@gmail.com>
2016-10-25 21:21:39 +0400

    wsregex_iterator

--- main.cpp
+++ main.cpp
@@ -12,98 +12,105 @@ using namespace boost::filesystem;
 
 struct occurrence
 {
-	int line, position;
-	wstring fileName, lineText, Text;
-	occurrence()
-	{}
-	occurrence(wstring fileName, wstring lineText, wstring Text, int line, int position)
-		:
-		fileName(fileName), lineText(lineText), Text(Text), line(line), position(position)
-	{}
+    int line, position;
+    wstring fileName, lineText, Text;
 
-	wstring toString()
-	{
-		return L"filename: " + fileName + L" , line text: " + lineText + L" , actual text: " + Text
-			+ L" , line number: " + std::to_wstring(line) + L" , position: " + std::to_wstring(position);
-	}
+    occurrence() {}
+
+    occurrence(wstring fileName, wstring lineText, wstring Text, int line, int position)
+            :
+            fileName(fileName), lineText(lineText), Text(Text), line(line), position(position) {}
+
+    wstring toString()
+    {
+        return L"filename: " + fileName + L" , line text: " + lineText + L" , actual text: " + Text
+               + L" , line number: " + std::to_wstring(line) + L" , position: " + std::to_wstring(position);
+    }
 };
 
 void findInFile(boost::filesystem::wifstream &stream, wstring filename, wregex &reg, vector<occurrence> &res)
 {
-	//vector<occurrence> res{};
-	res = vector<occurrence>();
-	wstring line;
-	int lineNum = 0;
-	while (std::getline(stream, line)) {
-		/*for (auto it = std::wcregex_iterator(line.c_str(), line.c_str() + sizeof(wchar_t) * line.size(), reg);
-			 it != std::wcregex_iterator(); ++it) {
+    //vector<occurrence> res{};
+    res = vector<occurrence>();
+    wstring line;
+    int lineNum = 0;
+    std::wsregex_iterator(line.begin(), line.end(), reg);
 
-			res.push_back(occurrence(filename, line, it->str(), lineNum, it->position()));
-			//cout << it->str() << endl;
-			//index_matches.push_back(it->position());
-		}*/
-		lineNum++;
 
-	}
-	cout <<"here"<<endl;
+
+    while (std::getline(stream, line))
+    {
+        for (auto it = std::wsregex_iterator(line.begin(), line.end(), reg);
+             it != std::wsregex_iterator(); ++it)
+        {
+
+            res.push_back(occurrence(filename, line, it->str(), lineNum, it->position()));
+            //cout << it->str() << endl;
+            //index_matches.push_back(it->position());
+        }
+        lineNum++;
+
+    }
+    //cout <<"here"<<endl;
 }
 
 optional<path> findInFiles(const path &dir_path, wregex &reg, vector<occurrence> &res)
 {
-	const recursive_directory_iterator end;
-	const auto it = find_if(recursive_directory_iterator(dir_path), end,
-							[&reg, &res](const directory_entry &e)
-							{
-								cout << e.path().string() << endl;
-								boost::filesystem::wifstream fin(e.path());
-								vector<occurrence> cur{};
-								findInFile(fin, e.path().wstring(), reg, cur);
-								/*for (int i = 0; i < cur.size(); i++) {
-									cout << cur[i].fileName << endl;
-									cout << cur[i].toString() << endl;
-								}*/
+    const recursive_directory_iterator end;
+    const auto it = find_if(recursive_directory_iterator(dir_path), end,
+                            [&reg, &res](const directory_entry &e)
+                            {
+                                cout << e.path().string() << endl;
+                                boost::filesystem::wifstream fin(e.path());
+                                vector<occurrence> cur{};
+                                findInFile(fin, e.path().wstring(), reg, cur);
+                                /*for (int i = 0; i < cur.size(); i++) {
+                                    cout << cur[i].fileName << endl;
+                                    cout << cur[i].toString() << endl;
+                                }*/
 
-								res.insert(res.end(), cur.begin(), cur.end());
+                                res.insert(res.end(), cur.begin(), cur.end());
 
-								return false;
-							});
-	return it == end ? optional<path>() : it->path();
+                                return false;
+                            });
+    return it == end ? optional<path>() : it->path();
 }
 
 int main()
 {
 
-	const path myPath = L"/cygdrive/c/cpp";
-	wregex reg(L"include");
-	vector<occurrence> res{};
-	findInFiles(myPath, reg, res);
+    const path myPath = L"/home/serg/cpp";
+    wregex reg(L"include");
+    vector<occurrence> res{};
+    findInFiles(myPath, reg, res);
 
-	for (int i = 0; i < res.size(); i++) {
-		cout << "filename: " << res[i].fileName << endl;
-		cout << "line text: " << res[i].lineText << endl;
-		cout << "found text: " << res[i].Text << endl;
-		cout << "line number: " << res[i].line << endl;
-		cout << "position: " << res[i].position << endl;
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << "filename: " << res[i].fileName << endl;
+        cout << "line text: " << res[i].lineText << endl;
+        cout << "found text: " << res[i].Text << endl;
+        cout << "line number: " << res[i].line << endl;
+        cout << "position: " << res[i].position << endl;
 
-		cout << endl;
-	}
-	cout << "overall " << res.size() << endl;
-	/*wstring str = L"տէստ տէստ ";
-	wregex rx(L"ստ");
+        cout << endl;
+    }
+    cout << "overall " << res.size() << endl;
+    /*wstring str = L"տէստ տէստ ";
+    wregex rx(L"ստ");
 
-	vector<int> index_matches; // results saved here
-	// (should be {2, 8}, but always get only {2})
+    vector<int> index_matches; // results saved here
+    // (should be {2, 8}, but always get only {2})
 
 
-	for (auto it = std::wcregex_iterator(str.c_str(), str.c_str() + sizeof(wchar_t) * str.size(), rx);
-		 it != std::wcregex_iterator();
-		 ++it) {
-		cout << it->str() << endl;
-		index_matches.push_back(it->position());
-	}
-	for (int i = 0; i < index_matches.size(); i++)
-		cout << index_matches[i] << " ";
-	cout << endl;*/
+    for (auto it = std::wcregex_iterator(str.c_str(), str.c_str() + sizeof(wchar_t) * str.size(), rx);
+         it != std::wcregex_iterator();
+         ++it) {
+        cout << it->str() << endl;
+        index_matches.push_back(it->position());
+    }
+    for (int i = 0; i < index_matches.size(); i++)
+        cout << index_matches[i] << " ";
+    cout << endl;*/
 
 
 
